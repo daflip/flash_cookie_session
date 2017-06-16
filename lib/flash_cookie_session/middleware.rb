f6b229680d770cdc2816680f585cbdcd07d3a2d3
Jerry Walsh <jerry.walsh@gmail.com>
2017-06-16 15:40:07 +0100

    apply cookie if there's no session_key specified in the cookie string (works better than if cookie is blank because the cookie could be populated but just not with our session_key!)

--- lib/flash_cookie_session/middleware.rb
+++ lib/flash_cookie_session/middleware.rb
@@ -2,6 +2,7 @@ module FlashCookieSession
   class Middleware
     USER_AGENT_MATCHER = /^(Adobe|Shockwave) Flash/.freeze
     HTTP_REFERER_MATCHER = /\.swf$/.freeze
+    SWFUPLOAD_MATCHER = /swfupload$/
 
     def initialize(app, session_key = Rails.application.config.session_options[:key])
       @app = app
@@ -11,19 +12,22 @@ module FlashCookieSession
     def call(env)
       referrer = env['HTTP_REFERER'].to_s.split('?').first
 
-      if env['HTTP_USER_AGENT'] =~ USER_AGENT_MATCHER || referrer =~ HTTP_REFERER_MATCHER
+      # if the agent matches, it's a .swf referer or the target path contains swfupload
+      if env['HTTP_USER_AGENT'] =~ USER_AGENT_MATCHER || referrer =~ HTTP_REFERER_MATCHER || env['PATH_INFO'] =~ SWFUPLOAD_MATCHER
         req = Rack::Request.new(env)
-        the_session_key = [ @session_key, req.params[@session_key] ].join('=').freeze if req.params[@session_key]
-
-        if req.params['remember_token'] && req.params['remember_token'] != 'null'
-          the_remember_token = [ 'remember_token', req.params['remember_token'] ].join('=').freeze
-        else
-          the_remember_token = nil
+        # if session key doesn't exist in cookie, but is included in the request array
+        if (not env['HTTP_COOKIE'].to_s.include?(@session_key)) and req.params[@session_key]
+          the_session_key = [ @session_key, req.params[@session_key] ].join('=').freeze 
+          if req.params['remember_token'] && req.params['remember_token'] != 'null'
+            the_remember_token = [ 'remember_token', req.params['remember_token'] ].join('=').freeze
+          else
+            the_remember_token = nil
+          end
+          cookie_with_remember_token_and_session_key = [ the_remember_token, the_session_key ].compact.join('\;').freeze
+          env['HTTP_COOKIE'] = cookie_with_remember_token_and_session_key 
+          puts "Applied cookie: #{env['HTTP_COOKIE']}"
+          puts "Applying session key: #{the_session_key.inspect}"
         end
-
-        cookie_with_remember_token_and_session_key = [ the_remember_token, the_session_key ].compact.join('\;').freeze
-
-        env['HTTP_COOKIE'] = cookie_with_remember_token_and_session_key if env['HTTP_COOKIE'].blank?
         env['HTTP_ACCEPT'] = "#{req.params['_http_accept']}".freeze if req.params['_http_accept']
       end
 
